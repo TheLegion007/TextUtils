6f2f641183be5aef3f108c139a843c3f0b9e25e3
Manish <manisksing@gmail.com>
2022-06-26 14:10:03 +0530

    Updates

---
+++ precache-manifest.0391a27b7a1d32cb6fd45ae30b16131f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "54a52c482c757ded9851",
+    "url": "/TextUtils/static/css/main.999762f6.chunk.css"
+  },
+  {
+    "revision": "54a52c482c757ded9851",
+    "url": "/TextUtils/static/js/main.54a52c48.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/TextUtils/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "242aac37736367b13e39",
+    "url": "/TextUtils/static/js/2.242aac37.chunk.js"
+  },
+  {
+    "revision": "13282edd1660c792f428",
+    "url": "/TextUtils/static/js/runtime~main.13282edd.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextUtils/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5715e80ef7a2ba332850ae4b3e7187e3",
+    "url": "/TextUtils/index.html"
+  }
+];
